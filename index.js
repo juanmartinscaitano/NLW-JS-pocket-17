1b4c78d3c1af81e7e281e517ecaf9124a0119a96
Juan Martins Caitano <juanmartinscaitano@gmail.com>
2024-09-10 15:26:19 -0300

    fundamentos 005

--- index.js
+++ index.js
@@ -17,10 +17,18 @@
 // console.log(metas[1].value)
 
 const start = () => {
-   let count = 1
-      while(count <= 10) {
-         console.log(count)
-         count++
+      while(true) {
+         let opcao = "cadastrar"
+         switch(opcao) {
+            case "cadastrar":
+               console.log("Vamos cadastrar")
+               break
+            case "listar":
+               console.log("Vamos listar")
+               break
+            case "sair":
+               return 
+         }
       }
 }
 
